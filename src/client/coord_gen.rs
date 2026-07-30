4263c9cbafbfe2ab0c41a2b740676fa19ca99f34
amedeo03 <amedeomarino03@gmail.com>
2026-07-30 12:14:21 +0200

    feat: implemented file-read coordinate generation

--- src/client/coord_gen.rs
+++ src/client/coord_gen.rs
@@ -1,7 +1,43 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+
 use G19::utils::coord::{Coordinates};
 
-pub fn get_next_coord() -> Coordinates {
-    let next= Coordinates::new(12.5, -5.1);
+pub struct CoordGenerator {
+    lines: Lines<BufReader<File>>,
+}
 
-    next
+impl CoordGenerator {
+    
+    pub fn init(path: &str) -> io::Result<Self> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        Ok(CoordGenerator {
+            lines: reader.lines(),
+        })
+    }
+
+    pub fn get_next(&mut self) -> Option<Coordinates> {
+        loop {
+            let line = self.lines.next()?;
+
+            let line = match line {
+                Ok(l) => l,
+                Err(_) => continue,
+            };
+
+            let line = line.trim();
+            if line.is_empty() {
+                continue;
+            }
+
+            let mut words = line.split_whitespace();
+            let lat = words.next().and_then(|s| s.parse::<f32>().ok());
+            let lon = words.next().and_then(|s| s.parse::<f32>().ok());
+
+            if let (Some(lat), Some(lon)) = (lat, lon) {
+                return Some(Coordinates::new(lat, lon));
+            }
+        }
+    }
 }
--- src/client/main.rs
+++ src/client/main.rs
@@ -1,8 +1,16 @@
-use crate::coord_gen::get_next_coord;
-
 mod coord_gen;
+use std::{thread, time};
 
-fn main() {
-    let next = get_next_coord();
-    println!("{:#?}", next)
+use crate::coord_gen::CoordGenerator;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut generator = CoordGenerator::init("./data/coordinates.txt")?;
+    
+    while let Some(coord) = generator.get_next() {
+        println!("{coord:?}");
+        thread::sleep(time::Duration::from_millis(30_000));
+    }
+
+    Ok(())
 }
