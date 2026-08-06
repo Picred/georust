c334921c5768d27da1fcd01fbe7064f4369ef3f9
Picred <andrei.stefand@outlook.com>
2026-08-06 19:00:46 +0200

    feat: begin statistics logic

--- src/server/main.rs
+++ src/server/main.rs
@@ -5,15 +5,28 @@ pub mod authenticator;
 pub mod database;
 pub mod models;
 pub mod repository;
+pub mod statistics;
 // -----------------------
 
 use std::env::args;
+
+use crate::statistics::{Statistics, RequiredTimeFrame};
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let _args: Vec<String> = args().collect();
 
-    println!("Server running");
+    let stats = Statistics::new(RequiredTimeFrame::CurrentDay);
+    // let mut stats = Statistics::new(RequiredTimeFrame::CurrentWeek);
+    // let mut stats = Statistics::new(RequiredTimeFrame::CurrentMonth);
 
+    // println!("Initial timeframe {:?}", stats.get_timeframe());
+    // stats.set_timeframe(RequiredTimeFrame::CurrentWeek);
+    // println!("Updated timeframe {:?}", stats.get_timeframe());
+
+
+    let range = stats.convert_timeframe_to_range();
+
+    println!("{:?}", range);
     Ok(())
 }
