fb05c7b33b1114eff7b65b38e8cfd83703b97f13
Picred <andrei.stefand@outlook.com>
2026-08-24 23:42:38 +0200

    chore: add log messages  to database  + user repository

--- src/server/main.rs
+++ src/server/main.rs
@@ -26,13 +26,14 @@ use std::time::Duration;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
-    Logger::init("logs/server.log", LogLevel::Info, Duration::from_secs(3))
+    Logger::init("logs/server.log", LogLevel::Debug, Duration::from_secs(3))
         .await
         .expect("failed to init logger");
 
     G19::info!(LogModule::Main, "startup", "Georust server starting up");
+    
 
-    let pool:Pool<Sqlite> = init_db(false).await?;
+    let pool:Pool<Sqlite> = init_db(true).await?;
     let manager = Arc::new(ConnectionManager::new(pool));
     let addr = "127.0.0.1:9001".to_string();
     let listener = TcpListener::bind(&addr).await?;
